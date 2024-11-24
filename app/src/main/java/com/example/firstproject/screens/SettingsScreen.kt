8b59394729b018c5b566eaca16dd53f2e80ac9a4
mka23 <mka230704@yandex.ru>
2024-11-24 18:55:45 +0300

    Сверстал settingsScreen

--- app/src/main/java/com/example/firstproject/screens/SettingsScreen.kt
+++ app/src/main/java/com/example/firstproject/screens/SettingsScreen.kt
@@ -1,16 +1,36 @@
 package com.example.firstproject.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.firstproject.R
 
 @Composable
 fun SettingScreen() {
+    val winOrange = Color(0xFFD05708)
     Box(
         modifier = Modifier.fillMaxSize()
     ) {
@@ -20,5 +40,92 @@ fun SettingScreen() {
             contentDescription = null,
             contentScale = ContentScale.Crop
         )
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.4f)
+                .padding(top = 26.dp),
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Row() {
+                Text(
+                    text = "WIN",
+                    color = Color.White,
+                    fontSize = 30.sp,
+                    fontStyle = FontStyle.Italic,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = "SPORT",
+                    color = winOrange,
+                    fontSize = 30.sp,
+                    fontStyle = FontStyle.Italic,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+
+
+            Text(
+                text = "НАСТРОЙКИ",
+                color = Color.White,
+                fontSize = 50.sp,
+                fontStyle = FontStyle.Italic,
+                fontWeight = FontWeight.Bold
+            )
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceAround
+            ) {
+                Text(
+                    text = "УВЕДОМЛЕНИЯ",
+                    color = winOrange,
+                    fontSize = 30.sp,
+                    fontStyle = FontStyle.Italic,
+                    fontWeight = FontWeight.Bold
+                )
+
+                val checkedState = remember { mutableStateOf(true) }
+                Switch(
+                    checked = checkedState.value,
+                    onCheckedChange = { checkedState.value = it },
+                    colors = SwitchDefaults.colors(
+                        checkedThumbColor = winOrange,
+                        checkedTrackColor = Color.White,
+                        uncheckedThumbColor = Color.Gray,
+                        uncheckedTrackColor = Color.LightGray,
+                    )
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .padding(end = 150.dp)
+                    .clickable { }
+            ) {
+                Text(
+                    text = "ОЧИСТИТЬ",
+                    color = winOrange,
+                    fontSize = 36.sp,
+                    fontStyle = FontStyle.Italic,
+                    fontWeight = FontWeight.Bold
+                )
+
+                Text(
+                    modifier = Modifier.padding(start = 14.dp),
+                    text = "ДАННЫЕ",
+                    color = winOrange,
+                    fontSize = 36.sp,
+                    fontStyle = FontStyle.Italic,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SettingScreen()
 }
